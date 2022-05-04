34476bd3fc9439ccf5fd7e54839e1d7b1548961f
Van Nguyen <99274250+van-nguyen2006@users.noreply.github.com>
2022-05-04 11:22:21 +0000

    Added more comments.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 'use strict';
 /**
- * This function 
+ * This function checks for the inputted age of the user and outputs back what movies they can see
  */
 function judge() {
   // Constants
@@ -16,26 +16,27 @@ function judge() {
   // Output message
   let message = ""
 
-  // 
+  // Ensures that the user inputted some sort of number
   if (age >= 0) {
+    // If the user is at least 18 years old
     if (age >= R_RATED) {
       message = "You can see R-rated movies and below alone!"
-      // 
+      // If the user is at least 13 years old
     } else if (age >= PG_THIRTEEN) {
       message = "You can only watch PG-13 Movies and below alone."
-      // 
-    } else if (age >= G_RATED_ALONE) {
+      // If the user is older than 10 years old (To see the movie alone)
+    } else if (age >= PG_RATED_ALONE) {
       message = "You can only see a G or PG movie alone"
-    } else if (age > 0) {
-      message = "You cannot see a movie alone! You child..."
+      // ELSE the user is younger than 10 years old
     } else {
-      message = "Fraudster...You're less than 1 years old? Right...How are you even on here?"
+      message = "You cannot see a movie alone! Since you are less than 10 years old."
     }
+    // ELSE the user didn't enter anything
   } else {
-    message = "Not inputting your age? You must think you're clever..."
+    message = "Please enter a valid age"
   }
     
 
-  // 
+  // Outputs what movies the user can see
   document.getElementById('viewable-movies').innerHTML = message
 }
